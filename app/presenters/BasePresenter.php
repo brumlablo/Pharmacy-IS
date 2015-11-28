bbbc94a29e4b9ed9668316ad1159cb224703f47e
Rog3ll <hamernik.pavel@seznam.cz>
2015-11-28 13:52:37 +0100

    Uprava na formular editace (Ajax atd)

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -21,4 +21,38 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
         $control->addMenuOption("Lék","Lek:");
         return $control;
     }
+    
+    
+    protected function createBootstrapForm(){
+        $form = new Nette\Application\UI\Form;
+        $renderer = $form->getRenderer();
+        
+        $renderer->wrappers['controls']['container'] = NULL;
+        $renderer->wrappers['pair']['container'] = 'div class=form-group';
+        $renderer->wrappers['pair']['.error'] = 'has-error';
+        $renderer->wrappers['label']['container'] = 'div class="col-sm-3 control-label"';
+        $renderer->wrappers['control'] = array(
+		'container' => 'div class=col-sm-9',
+		'.odd' => NULL,
+
+		'description' => 'span class=help-block',
+		'requiredsuffix' => '',
+		'errorcontainer' => 'span class=help-block',
+		'erroritem' => '',
+
+		'.required' => 'required',
+		'.text' => 'form-control',
+		'.password' => 'form-control',
+		'.file' => 'text',
+		'.submit' => "btn btn-success",
+		'.image' => 'imagebutton',
+		'.button' => "btn btn-default",
+	);
+        
+        $form->getElementPrototype()
+                ->class('form-horizontal page-form')
+                ->role('form');
+        return $form;
+        
+    }
 }
--- app/presenters/LekPresenter.php
+++ app/presenters/LekPresenter.php
@@ -30,4 +30,70 @@ class LekPresenter extends BasePresenter{
     {
         $this->template->leky = $this->database->table('lek');
     }
+    /*
+    public function renderEdit($id){
+        $this->template->lek = $this->database->table('lek')->get($idLek);
+    }*/
+
+    public function renderEdit($id){
+        
+        $this->template->setFile(__DIR__.'/templates/Lek/edit.latte');
+        $this->template->lek = $this->database->table('lek')->get($id);
+        $this['lekEditForm']->setDefaults($this->template->lek);
+    }
+    
+    public function handleEdit($idLek){      
+        if($this->isAjax()){
+            $this->template->setFile(__DIR__.'/templates/Lek/edit.latte');
+            $this->template->lek = $this->database->table('lek')->get($idLek);
+            $this['lekEditForm']->setDefaults($this->template->lek);
+            $this->redrawControl("modalcontent");
+        } else {
+            $this->redirect("Lek:edit", $idLek);
+        }
+    }
+    
+    
+    public function createComponentLekEditForm(){
+        $form = $this->createBootstrapForm();
+        $form->addText('ean_id',"EAN:");
+        $form->addText('nazev',"Název:");
+        $form->addTextArea('popis',"Popis:",0, 5)
+                ->getControlPrototype()->class("form-control");
+        $form->addSubmit("save", "Uložit");
+        //$form->addButton("cancel", "Zrušit",new Nette\Application\Link($this, 'Lek:default'));
+        
+        $form["modal_cancel"] = new CancelButton("Zrušit", $this->link("Lek:default"));
+        
+        $form->getElementPrototype()
+                ->class('form-horizontal page-form')
+                ->role('form');
+        
+        return $form;
+    }
+    
+    
+}
+
+
+class CancelButton extends Nette\Forms\Controls\Button{
+    
+    private $href;
+    
+    public function __construct($caption = NULL,$href="NULL") {
+        parent::__construct($caption);
+        $this->href = $href;
+        $this->control = Nette\Utils\Html::el('a',NULL);
+    }
+    
+    public function getLabel($caption = NULL) {
+        return NULL;
+    }
+    public function getControl($caption = NULL) {
+        $this->setOption('rendered',TRUE);
+        $el = clone $this->control;
+        return $el->class($this->getHtmlName()." btn btn-default")
+                ->href($this->href)
+                ->add($caption === NULL? $this->caption:$caption);
+    }
 }
